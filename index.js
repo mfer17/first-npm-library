ec8a86edd2cdca1e448f52580b3e2a1fba962f44
Maria Fernanda Monoga Mejia <desarrolladorweb4@kronotime.com.co>
2025-06-04 20:00:38 -0500

    initial commit: add index.js and package.json for first npm library

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    hola: function() {
+        console.log("Hola, mundo!");
+    },
+    holaPersonalizado: function(nombre){
+        console.log(`Hola, ${nombre}!`);
+    }
+}
